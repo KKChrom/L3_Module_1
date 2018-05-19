b2e7b03ff77d6d700b5eae5b4c57ff60642f83da
league <league@wts7.attlocal.net>
2018-05-19 14:26:17 -0700

    algorithms again

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package SearchingAndSortingAlgorithms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Algorithms {
@@ -20,12 +21,63 @@ public class Algorithms {
 		int x=0;
 		for(int i =0; i<oysters.size();i++)
 		{
-			if(oysters.get(i).equals("true"))
+			if(oysters.get(i).equals(true))
 			{
 				x++;
 			}
 			
 		}
 		return x;
+	}
+	public static double findTallest(List<Double> peeps)
+	{
+		double x = 0;
+		for(int i = 0;i<peeps.size();i++)
+		{
+			if(x<peeps.get(i))
+			{
+				x=peeps.get(i);
+			}
+			
+		}
+		return x;
+	}
+	public static String findLongestWord(List<String> words)
+	{
+		String x = "";
+		for(int i = 0;words.size()>i;i++)
+		{
+			if(x.length()<words.get(i).length())
+			{
+				x = words.get(i);
+			}
+		}
+		return x;
+	}
+	public static boolean containsSOS(List<String> message)
+	{
+		for(int i =0; message.size()> i; i++)
+		{
+			if(message.get(i).contains("... --- ..."))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	public static List<Double> sortScores(List<Double> results)
+	{
+		 Collections.sort(results);
+		 return results;
+	}
+	public static List<String> sortDNA(List<String> unsortedSequences)
+	{
+		Collections.sort(unsortedSequences);
+		return unsortedSequences;
+	}
+	public static List <String>sortWords (List<String> words)
+	{
+		java.util.Collections.sort(words);
+		return words;
 	}
 }
